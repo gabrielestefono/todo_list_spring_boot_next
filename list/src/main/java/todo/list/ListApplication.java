efd03bc27d1dd76f680c49923457cd701a9cc6c6
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-09 21:37:18 -0300

    Criando CRUD tasks local

--- list/src/main/java/todo/list/ListApplication.java
+++ list/src/main/java/todo/list/ListApplication.java
@@ -3,11 +3,10 @@ package todo.list;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "todo.controllers", "todo" })
 public class ListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ListApplication.class, args);
 	}
-
 }
